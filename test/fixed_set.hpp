e560a051c21ab1c44a92c3c29fb7d7667bdef399
dementive <87823030+dementive@users.noreply.github.com>
2025-09-06 00:32:06 -0500

    Set test tweak

--- test/fixed_set.hpp
+++ test/fixed_set.hpp
@@ -1,19 +1,19 @@
 #include "lt/defs/test.hpp"
 #include "lt/fixed_set.hpp"
 
-inline lt::fixed_set<int, 6> set = { 1, 2, 3, 4, 5 };
+inline lt::fixed_set<int, 7> set = { 1, 2, 3, 4, 5 };
 
 test(fixed_set,
 	"operator[]", set[2] == 3,
 	"operator[] assignment", set[3] = 999; set[3] == 999,
 	"at", set[4] == 5,
 	"size", set.size() == 5,
-	"push_back", set.push_back(6); set.size() == 6,
-	"pop_back", set.pop_back(); set.size() == 5,
+	"push_back", auto t = [&](){ set.push_back(6); set.push_back(8); return set.size() == 7; }; t(),
+	"pop_back", set.pop_back(); set.size() == 6,
 	"front", set.front() == 1,
-	"back", set.back() == 5,
+	"back", set.back() == 6,
 	"begin", *set.begin() == 1,
-	"end", *(set.end() - 1) == 5,
+	"end", *(set.end() - 1) == 6,
 	"has", set.has(1),
 	"erase", set.erase(1); !set.has(1)
 )
