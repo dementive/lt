17a602ded86ac5367d272f141bb7d1997703e548
dementive <87823030+dementive@users.noreply.github.com>
2025-09-13 23:38:59 -0500

    range tweak

--- src/lt/types/range.hpp
+++ src/lt/types/range.hpp
@@ -4,10 +4,10 @@ namespace lt {
 
 template <typename T>
 concept Range = requires(T p_t) {
-	{ p_t.begin() };
-	{ p_t.end() };
-	{ p_t.size() };
-	{ p_t[0] };
+	p_t.begin();
+	p_t.end();
+	p_t.size();
+	p_t[0];
 };
 
 } // namespace lt
