80495847223b537a549e3bdea05ab282f375cd8a
dementive <87823030+dementive@users.noreply.github.com>
2025-10-05 11:27:41 -0500

    Add missing pair operators

--- src/lt/pair.hpp
+++ src/lt/pair.hpp
@@ -5,7 +5,13 @@ namespace lt {
 template <typename F, typename S> struct pair {
 	F first{};
 	S second{};
+
 	constexpr bool operator==(const pair &p_other) const { return first == p_other.first && second == p_other.second; }
+	constexpr bool operator!=(const pair &p_other) const { return first != p_other.first || second != p_other.second; }
+	constexpr bool operator<(const pair &p_other) const { return first == p_other.first ? (second < p_other.second) : (first < p_other.first); }
+	constexpr bool operator<=(const pair &p_other) const { return first == p_other.first ? (second <= p_other.second) : (first < p_other.first); }
+	constexpr bool operator>(const pair &p_other) const { return first == p_other.first ? (second > p_other.second) : (first > p_other.first); }
+	constexpr bool operator>=(const pair &p_other) const { return first == p_other.first ? (second >= p_other.second) : (first > p_other.first); }
 };
 
 } // namespace lt
